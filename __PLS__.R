a0e4ee3546aed36149407053b44e66cd1d80f478
El Bouga Latifa <latifaelbouga1999@gmail.com>
2022-12-02 11:59:19 +0100

    Add files via upload

--- __PLS__.R
+++ __PLS__.R
@@ -1,16 +1,20 @@
+# Les packages utilisées
 library(Hmisc)
 library(MASS)
 library(data.table)
 library(rprojroot)
 
+#=================================== La fonction PLS ==============================================
+
 PLS<-function(data,Y,pvalue){
 
-  # pour extraire les indices des variable pour la construction du T1
+  # Fonction d'extraction des variable significatif pour la construction du T1
   T1_vars_indices <- function(data, pvalue){
     verified <- c()
     i <- 1
     # extraction des correlations et les P values
     rc <- rcorr(data, type=c("pearson","spearman"))
+    
     # pour chaque p value on verifie si elle est inferieur a la valeur de P value entrée
     for(x in rc$P[1:nrow(rc$P) - 1,"Y"]){
       if(abs(x) < pvalue && x != 1){
@@ -20,7 +24,8 @@ PLS<-function(data,Y,pvalue){
     }
     return(verified)
   }
-  # Calcule de risdu
+  
+  # Fonction du Calcule des residus
   residu<- function(DT_scale, verified){
     rn <- as.data.frame(matrix(nrow = length(DT_scale[,"Y_cn"]), ncol = length(verified)))
     colnames(rn) <- paste(verified, "res", sep="_")
@@ -32,7 +37,8 @@ PLS<-function(data,Y,pvalue){
     }
     return(rn)
   }
-  # residu normé
+  
+  # Fonction du calcul des residus normés
   residu_norm <- function(r){
     temp <- r
     for(x in colnames(temp)){
@@ -47,7 +53,8 @@ PLS<-function(data,Y,pvalue){
 
   # Lecture des données
   data = as.data.frame(data)
-  # extraction de la variable Y
+  
+  # extraction de la variable explicatif Y
   y = data[,paste(Y)]
   data[Y] = NULL
 
@@ -61,10 +68,10 @@ PLS<-function(data,Y,pvalue){
 
   f <- paste("Y ~ 0 + ", paste(names, collapse=" + "),collapse='+')
 
-  # Premiere regression
+  # Premiere regression multiple
   lm <- lm(formula = f, data = data)
 
-  # Extraction des corrélations et les pvalues
+  # Extraction des corrélations et les p-values
   dtm<-as.matrix(data)
   rcorr(dtm, type=c("pearson","spearman"))
 
@@ -77,32 +84,38 @@ PLS<-function(data,Y,pvalue){
   
   
   verified <- T1_vars_indices(data = dtm, pvalue)
+  
   #========================================================
-  #calculate T1
+  #################Construction de T1 ###########################
+  
   # first term in T1
   A <- 0
   for (x in colnames(dtms)[verified]){
     A <- A + cov_m["Y_cn",x] ^ 2
   }
+  
   # second term in T1
   B <- 0
   for (x in colnames(dtms)[verified]){
     B <- B + dtms[,x] * cov_m["Y_cn",x]
   }
+  
   # T1
   T1 = B / sqrt(A)
   T = as.data.frame(T1)
   DT_scale <- cbind(DT_scale, T)
   
-  # L'explication des T en function des variables
+  # Passage du T vers les variables d'origine
   Texp = c()
   T1formula = "T1 = 0"
   for (x in colnames(dtms)[verified]){
     T1formula <- paste(T1formula, paste(x , cov_m["Y_cn",x],  sep = " * "), sep = " + ")
   }
   Texp = append(Texp , T1formula)
-  # Construction des composantes T
-  #========================================================
+  
+ 
+  ######################### Construction des composantes T ##########################
+  
   k <- 2
   repeat{
     
@@ -117,7 +130,7 @@ PLS<-function(data,Y,pvalue){
     DT_scale <- cbind(DT_scale, dtms)
     DT_scale <- cbind(DT_scale, T)
     
-    # Extraction des variables pour la construction de Tk
+    # Extraction des variables pour la construction de Ti
     verified <- c()
     for(i in 1:(length(colnames(dtms)) - 1) ){
       f <- paste("Y_cn ~ 0 + ", paste(paste(colnames(T), collapse=" + "), "+" ),paste(colnames(dtms)[i], collapse=" + ") )
@@ -132,6 +145,7 @@ PLS<-function(data,Y,pvalue){
 
     #========================================================
     # Condition d'arret
+    
     if(length(verified) == 0){
       break
     }
@@ -153,18 +167,22 @@ PLS<-function(data,Y,pvalue){
     }
     verified_
     coef_
+    
+    #################Construction de Ti ###########################
     # first term in Ti
     A <- 0
     for (x in coef_){
       A <- A + (x * x)
     }
     A <- sqrt(A)
+    
     # second term in Ti
     B <- 0
     for (i in 1:length(verified_)){
       
       B <- B + (coef_[i] * r[,verified_[i]]) 
     }
+    
     assign(paste("T",k, sep=""), B / A)
     T <- cbind(T,get(paste("T",k, sep="")))
     z = 1
@@ -177,27 +195,30 @@ PLS<-function(data,Y,pvalue){
     
     k<-k+1
   }
+  # La data frame finale
   res = list('T_table'= DT_scale,"T_exp"= Texp)
   return(res)
 }
 
-# get of this file path
+#=================================== Le test ==============================================
 
-#path2data<-file.path("c:","Users","KINTARO","STUDIES","s3","Analyse de donees","RegPLS_R")
-
+# get of current file path
 path2data = find_rstudio_root_file()
-# read data
+
+################# data: Data_Cornell.csv ########################
 DM.dt <- fread(file.path(path2data, "Data_Cornell.csv"))
+
+
+corres = PLS(DM.dt,'Y',0.1) # L'appel de la fonction PLS
+Tcor = corres$T_table       # Data Frame qui contient les variables origines + Les Ti
+Tcorexp = corres$T_exp      # Les variables origines contribuent à chaque Ti
+
+################# data: mtcars.csv ########################
 mtcars <- fread(file.path(path2data,"mtcars.csv"))
+mtcarres = PLS(mtcars[,-c(1)],'mpg',0.1)      # L'appel de la fonction PLS
 
-corres = PLS(DM.dt,'Y',0.05)
-Tcor = corres$T_table
-Tcorexp = corres$T_exp
-
-mtcarres = PLS(mtcars[,-c(1)],'mpg',0.1)
-
-Tmtcar = mtcarres$T_table
-Tmtcarexp = mtcarres$T_exp
+Tmtcar = mtcarres$T_table    # Data Frame qui contient les variables origines + Les Ti 
+Tmtcarexp = mtcarres$T_exp   # Les variables origines contribuent à chaque Ti
 
 Tmtcarexp = as.data.frame(Tmtcarexp)
 Tcorexp = as.data.frame(Tcorexp)
